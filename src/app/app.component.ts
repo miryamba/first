7ba20fa3c9afdd3e3791b4d1b01c24a5aa176236
miryamba <miryam2034@gmail.com>
2021-11-18 11:18:19 +0200

    4 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component, VERSION } from '@angular/core';
-
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
+export class AppComponent {
   name = 'Angular ' + VERSION.major;
+  constructor(
+    private fb: FormBuilder,
+    private dialogRef: MatDialogRef<dialogData>,
+    @Inject(MAT_DIALOG_DATA) public data: any
+  ) {
+    this.issPosition = data;
+  }
 }
