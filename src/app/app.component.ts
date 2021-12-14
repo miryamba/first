c869b7c05c7d17f1f53755b96e9bfd0751aba233
miryamba <miryam2034@gmail.com>
2021-12-14 10:23:14 +0200

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,13 @@
-import { Component, VERSION, Inject ,OnInit} from '@angular/core';
-import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
+import { Component, VERSION, Inject, OnInit } from '@angular/core';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit  {
+export class AppComponent implements OnInit {
   name = 'Angular ' + VERSION.major;
-  constructor(
-    
-    @Inject(MAT_DIALOG_DATA) public data: any) {
-   
-}
-ngOnInit() {}
+  constructor() {}
+
+  ngOnInit() {}
 }
